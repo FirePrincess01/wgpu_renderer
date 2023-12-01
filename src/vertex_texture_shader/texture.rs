6b20509180e6734d2e2dc203d188add3ea927b25
CeciliaSandbox <130098384+CeciliaSandbox@users.noreply.github.com>
2023-12-01 21:22:55 +0100

    Simplified texture construction

--- src/vertex_texture_shader/texture.rs
+++ src/vertex_texture_shader/texture.rs
@@ -22,59 +22,7 @@ impl Texture {
         rgba: &image::ImageBuffer<image::Rgba<u8>, Vec<u8>>, 
         label: Option<&str>
     ) -> Result<Self> {
-        let dimensions = rgba.dimensions();
-        let size = wgpu::Extent3d {
-            width: dimensions.0,
-            height: dimensions.1,
-            depth_or_array_layers: 1,
-        };
-
-        let texture = wgpu_renderer.device().create_texture(
-            &wgpu::TextureDescriptor {
-                label,
-                size,
-                mip_level_count: 1,
-                sample_count: 1,
-                dimension: wgpu::TextureDimension::D2,
-                format: wgpu::TextureFormat::Rgba8UnormSrgb,
-                usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-                view_formats: &[],
-            }
-        );
-
-        Self::write_texture(wgpu_renderer.queue(), &texture, &rgba, 1);
-
-        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let sampler = wgpu_renderer.device().create_sampler(
-            &wgpu::SamplerDescriptor {
-                address_mode_u: wgpu::AddressMode::ClampToEdge,
-                address_mode_v: wgpu::AddressMode::ClampToEdge,
-                address_mode_w: wgpu::AddressMode::ClampToEdge,
-                mag_filter: wgpu::FilterMode::Linear,
-                min_filter: wgpu::FilterMode::Nearest,
-                mipmap_filter: wgpu::FilterMode::Nearest,
-                ..Default::default()
-            }
-        );
-
-        let bind_group = wgpu_renderer.device().create_bind_group(
-            &wgpu::BindGroupDescriptor {
-                layout: texture_bind_group_layout.get(),
-                entries: &[
-                    wgpu::BindGroupEntry {
-                        binding: 0,
-                        resource: wgpu::BindingResource::TextureView(&view),
-                    },
-                    wgpu::BindGroupEntry {
-                        binding: 1,
-                        resource: wgpu::BindingResource::Sampler(&sampler),
-                    }
-                ],
-                label: Some("texture_bind_group"),
-            }
-        );
-
-        Ok(Self { texture, view, sampler, bind_group, nr_mipmaps: 1 })
+        Self::new_with_mipmaps(wgpu_renderer, texture_bind_group_layout, rgba, label, 1)
     }
 
     pub fn new_with_mipmaps(
