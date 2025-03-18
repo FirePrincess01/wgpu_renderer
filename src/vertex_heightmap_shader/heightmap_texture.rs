5bfc73834d3a5fc92bb9b8dfcdb2c1016fdc89db
FirePrincess01 <SarahSteinbrecher01@gmx.at>
2025-03-18 19:11:27 +0100

    kinda works

--- src/vertex_heightmap_shader/heightmap_texture.rs
+++ src/vertex_heightmap_shader/heightmap_texture.rs
@@ -1,6 +1,8 @@
 //! The HeightmapTexture used in the shader
 //!
 
+use crate::wgpu_renderer::WgpuRendererInterface;
+
 use super::{Heightmap, HeightmapBindGroupLayout};
 
 pub struct HeightmapTexture {
@@ -12,9 +14,9 @@ pub struct HeightmapTexture {
 
 impl HeightmapTexture {
     pub fn new(
-        device: &wgpu::Device,
+        renderer: &mut dyn WgpuRendererInterface,
         heightmap_bind_group_layout: &HeightmapBindGroupLayout,
-        _heightmap: &[Heightmap],
+        heightmap: &[Heightmap],
         width: u32,
         height: u32,
         label: Option<&str>,
@@ -25,7 +27,7 @@ impl HeightmapTexture {
             depth_or_array_layers: 1,
         };
 
-        let texture = device.create_texture(&wgpu::TextureDescriptor {
+        let texture = renderer.device().create_texture(&wgpu::TextureDescriptor {
             label,
             size,
             mip_level_count: 1,
@@ -38,7 +40,7 @@ impl HeightmapTexture {
 
         let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
 
-        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        let bind_group = renderer.device().create_bind_group(&wgpu::BindGroupDescriptor {
             layout: heightmap_bind_group_layout.get(),
             entries: &[wgpu::BindGroupEntry {
                 binding: 0,
@@ -46,6 +48,8 @@ impl HeightmapTexture {
             }],
             label: Some("texture_bind_group"),
         });
+
+        Self::do_update(renderer.queue(), heightmap, width, height, &texture);
 
         Self {
             texture,
@@ -55,10 +59,11 @@ impl HeightmapTexture {
         }
     }
 
-    pub fn update(&self, queue: &wgpu::Queue, heightmap: &[Heightmap]) {
+    fn do_update(queue: &wgpu::Queue, heightmap: &[Heightmap], width: u32, height: u32, texture: &wgpu::Texture)
+    {
         let size = wgpu::Extent3d {
-            width: self.width,
-            height: self.height,
+            width: width,
+            height: height,
             depth_or_array_layers: 1,
         };
 
@@ -67,18 +72,46 @@ impl HeightmapTexture {
         queue.write_texture(
             wgpu::TexelCopyTextureInfo {
                 aspect: wgpu::TextureAspect::All,
-                texture: &self.texture,
+                texture: texture,
                 mip_level: 0,
                 origin: wgpu::Origin3d::ZERO,
             },
             data,
             wgpu::TexelCopyBufferLayout {
                 offset: 0,
-                bytes_per_row: Some(4 * self.width),
-                rows_per_image: Some(self.height),
+                bytes_per_row: Some(4 * width),
+                rows_per_image: Some(height),
             },
             size,
         );
+    }
+
+    pub fn update(&self, queue: &wgpu::Queue, heightmap: &[Heightmap]) {
+        Self::do_update(queue, heightmap, self.width, self.height, &self.texture);
+
+        // let size = wgpu::Extent3d {
+        //     width: self.width,
+        //     height: self.height,
+        //     depth_or_array_layers: 1,
+        // };
+
+        // let data = bytemuck::cast_slice(heightmap);
+
+        // queue.write_texture(
+        //     wgpu::TexelCopyTextureInfo {
+        //         aspect: wgpu::TextureAspect::All,
+        //         texture: &self.texture,
+        //         mip_level: 0,
+        //         origin: wgpu::Origin3d::ZERO,
+        //     },
+        //     data,
+        //     wgpu::TexelCopyBufferLayout {
+        //         offset: 0,
+        //         bytes_per_row: Some(4 * self.width),
+        //         rows_per_image: Some(self.height),
+        //     },
+        //     size,
+        // );
     }
 
     pub fn bind<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>) {
