44a601dd22432c1ddd8401a45fbaf6af9a6997de
Sarah <SarahSteinbrecher01@gmx.at>
2025-12-24 18:12:42 +0100

    added scale_factor to graph

--- src/performance_monitor/graph.rs
+++ src/performance_monitor/graph.rs
@@ -5,6 +5,20 @@ use super::super::vertex_color_shader::Color;
 use super::super::vertex_color_shader::Vertex;
 use super::watch;
 
+struct Geometry {
+    len_per_micro: f32,
+
+    offset_x: usize,
+    offset_y: usize,
+
+    width: usize,
+    height: usize,
+    nr_lines: usize,
+    _line_length: usize,
+
+    fps_lines: [Vertex; 6],
+}
+
 pub struct Graph<const SIZE: usize> {
     pub vertices: Vec<Vertex>,
     pub colors: Vec<Color>,
@@ -12,6 +26,9 @@ pub struct Graph<const SIZE: usize> {
 
     watch_points_size: usize,
     color_gradient: colorous::Gradient,
+
+    geometry: Geometry,
+
 }
 
 impl<const SIZE: usize> Graph<SIZE> {
@@ -19,76 +36,93 @@ impl<const SIZE: usize> Graph<SIZE> {
     const DURATION_60FPS: instant::Duration = std::time::Duration::from_micros(16666); // 60 fps
     const DURATION_30FPS: instant::Duration = std::time::Duration::from_micros(33333); // 30 fps
 
-    const LEN_PER_MICRO: f32 = Self::LINE_LENGTH as f32 / Self::DURATION_30FPS.as_micros() as f32;
+    fn create_geometry(scale_factor: f32) -> Geometry {
+        
+        const OFFSET_X: usize = 10;
+        const OFFSET_Y: usize = 10;
 
-    const OFFSET_X: usize = 10;
-    const OFFSET_Y: usize = 10;
+        let width: usize = (700 as f32 * scale_factor) as usize;
+        let height: usize = (210 as f32 * scale_factor) as usize;
+        let nr_lines: usize = width - OFFSET_X;
+        let line_length: usize = height - OFFSET_Y;
+    
 
-    const WIDTH: usize = 700;
-    const HEIGHT: usize = 210;
-    const NR_LINES: usize = Self::WIDTH - (Self::OFFSET_X);
-    const LINE_LENGTH: usize = Self::HEIGHT - Self::OFFSET_Y;
+        let len_per_micro: f32 = line_length as f32 / Self::DURATION_30FPS.as_micros() as f32;
 
-    const FPS_LINES: &'static [Vertex] = &[
-        Vertex {
-            position: [Self::OFFSET_X as f32, Self::OFFSET_Y as f32, 0.0],
-        },
-        Vertex {
-            position: [
-                Self::OFFSET_X as f32 + Self::NR_LINES as f32,
-                Self::OFFSET_Y as f32,
-                0.0,
-            ],
-        },
-        Vertex {
-            position: [
-                Self::OFFSET_X as f32,
-                Self::OFFSET_Y as f32
-                    + Self::LEN_PER_MICRO * Self::DURATION_120FPS.as_micros() as f32,
-                0.0,
-            ],
-        },
-        Vertex {
-            position: [
-                Self::OFFSET_X as f32 + Self::NR_LINES as f32,
-                Self::OFFSET_Y as f32
-                    + Self::LEN_PER_MICRO * Self::DURATION_120FPS.as_micros() as f32,
-                0.0,
-            ],
-        },
-        Vertex {
-            position: [
-                Self::OFFSET_X as f32,
-                Self::OFFSET_Y as f32
-                    + Self::LEN_PER_MICRO * Self::DURATION_60FPS.as_micros() as f32,
-                0.0,
-            ],
-        },
-        Vertex {
-            position: [
-                Self::OFFSET_X as f32 + Self::NR_LINES as f32,
-                Self::OFFSET_Y as f32
-                    + Self::LEN_PER_MICRO * Self::DURATION_60FPS.as_micros() as f32,
-                0.0,
-            ],
-        },
-        // Vertex {
-        //     position: [
-        //         Self::OFFSET_X as f32,
-        //         Self::OFFSET_Y as f32
-        //             + Self::LEN_PER_MICRO * Self::DURATION_30FPS.as_micros() as f32,
-        //         0.0,
-        //     ],
-        // },
-        // Vertex {
-        //     position: [
-        //         Self::OFFSET_X as f32 + Self::NR_LINES as f32,
-        //         Self::OFFSET_Y as f32
-        //             + Self::LEN_PER_MICRO * Self::DURATION_30FPS.as_micros() as f32,
-        //         0.0,
-        //     ],
-        // },
-    ];
+
+
+        let fps_lines: [Vertex; 6] = [
+            Vertex {
+                position: [OFFSET_X as f32, OFFSET_Y as f32, 0.0],
+            },
+            Vertex {
+                position: [
+                    OFFSET_X as f32 + nr_lines as f32,
+                    OFFSET_Y as f32,
+                    0.0,
+                ],
+            },
+            Vertex {
+                position: [
+                    OFFSET_X as f32,
+                    OFFSET_Y as f32
+                        + len_per_micro * Self::DURATION_120FPS.as_micros() as f32,
+                    0.0,
+                ],
+            },
+            Vertex {
+                position: [
+                    OFFSET_X as f32 + nr_lines as f32,
+                    OFFSET_Y as f32
+                        + len_per_micro * Self::DURATION_120FPS.as_micros() as f32,
+                    0.0,
+                ],
+            },
+            Vertex {
+                position: [
+                    OFFSET_X as f32,
+                    OFFSET_Y as f32
+                        + len_per_micro * Self::DURATION_60FPS.as_micros() as f32,
+                    0.0,
+                ],
+            },
+            Vertex {
+                position: [
+                    OFFSET_X as f32 + nr_lines as f32,
+                    OFFSET_Y as f32
+                        + len_per_micro * Self::DURATION_60FPS.as_micros() as f32,
+                    0.0,
+                ],
+            },
+            // Vertex {
+            //     position: [
+            //         OFFSET_X as f32,
+            //         OFFSET_Y as f32
+            //             + len_per_micro * Self::DURATION_30FPS.as_micros() as f32,
+            //         0.0,
+            //     ],
+            // },
+            // Vertex {
+            //     position: [
+            //         OFFSET_X as f32 + nr_lines as f32,
+            //         OFFSET_Y as f32
+            //             + len_per_micro * Self::DURATION_30FPS.as_micros() as f32,
+            //         0.0,
+            //     ],
+            // },
+        ];
+
+        return Geometry {
+            len_per_micro,
+            offset_x: OFFSET_X,
+            offset_y: OFFSET_Y,
+            width,
+            height,
+            nr_lines,
+            _line_length: line_length,
+            fps_lines,
+        }
+    }
 
     pub fn color_gradient_vec(
         color_gradient: &colorous::Gradient,
@@ -114,18 +148,20 @@ impl<const SIZE: usize> Graph<SIZE> {
         Self::color_gradient_vec(&self.color_gradient, self.watch_points_size)
     }
 
-    pub fn new(color_gradient: colorous::Gradient) -> Self {
+    pub fn new(color_gradient: colorous::Gradient, scale_factor: f32) -> Self {
         let watch_points_size = SIZE;
         let line_nr_vertices = watch_points_size * 2 + 2;
 
+        let geometry = Self::create_geometry(scale_factor);
+
         let mut vertices =
-            vec![Vertex::zero(); line_nr_vertices * Self::NR_LINES + Self::FPS_LINES.len()];
+            vec![Vertex::zero(); line_nr_vertices * geometry.nr_lines + geometry.fps_lines.len()];
         let mut colors =
-            vec![Color::white(); line_nr_vertices * Self::NR_LINES + Self::FPS_LINES.len()];
-        let mut indices = vec![0_u32; line_nr_vertices * Self::NR_LINES + Self::FPS_LINES.len()];
+            vec![Color::white(); line_nr_vertices * geometry.nr_lines + geometry.fps_lines.len()];
+        let mut indices = vec![0_u32; line_nr_vertices * geometry.nr_lines + geometry.fps_lines.len()];
 
         // vertices
-        vertices[..Self::FPS_LINES.len()].copy_from_slice(Self::FPS_LINES);
+        vertices[..geometry.fps_lines.len()].copy_from_slice(&geometry.fps_lines);
 
         // colors
         let color_gradient_vec = Self::color_gradient_vec(&color_gradient, watch_points_size);
@@ -134,16 +170,16 @@ impl<const SIZE: usize> Graph<SIZE> {
             for j in 0..watch_points_size {
                 let color = color_gradient_vec[j];
 
-                colors[Self::FPS_LINES.len() + i * line_nr_vertices + j * 2].color = color.into();
-                colors[Self::FPS_LINES.len() + i * line_nr_vertices + j * 2 + 1].color =
+                colors[geometry.fps_lines.len() + i * line_nr_vertices + j * 2].color = color.into();
+                colors[geometry.fps_lines.len() + i * line_nr_vertices + j * 2 + 1].color =
                     color.into();
             }
 
             // last two points are gray (show the combined fps)
             let gray = [0.5, 0.5, 0.5];
-            colors[Self::FPS_LINES.len() + i * line_nr_vertices + line_nr_vertices - 2].color =
+            colors[geometry.fps_lines.len() + i * line_nr_vertices + line_nr_vertices - 2].color =
                 gray;
-            colors[Self::FPS_LINES.len() + i * line_nr_vertices + line_nr_vertices - 1].color =
+            colors[geometry.fps_lines.len()+ i * line_nr_vertices + line_nr_vertices - 1].color =
                 gray;
         }
 
@@ -159,10 +195,13 @@ impl<const SIZE: usize> Graph<SIZE> {
 
             watch_points_size,
             color_gradient,
+
+            geometry,
         }
     }
 
     fn create_line(
+        geometry: &Geometry,
         last_update_time: instant::Instant,
         update_time: instant::Instant,
         watch_points: &[watch::WatchPoint],
@@ -174,13 +213,13 @@ impl<const SIZE: usize> Graph<SIZE> {
             let j = i * 2;
 
             let micros_start = if watch_point.start > last_update_time {
-                (watch_point.start - last_update_time).as_micros() as f32 * Self::LEN_PER_MICRO
+                (watch_point.start - last_update_time).as_micros() as f32 * geometry.len_per_micro
             } else {
                 0.0
             };
 
             let micros_stop = if watch_point.start > last_update_time {
-                (watch_point.stop - last_update_time).as_micros() as f32 * Self::LEN_PER_MICRO
+                (watch_point.stop - last_update_time).as_micros() as f32 * geometry.len_per_micro
             } else {
                 0.0
             };
@@ -190,7 +229,7 @@ impl<const SIZE: usize> Graph<SIZE> {
         }
 
         let micros = if update_time > last_update_time {
-            (update_time - last_update_time).as_micros() as f32 * Self::LEN_PER_MICRO
+            (update_time - last_update_time).as_micros() as f32 * geometry.len_per_micro
         } else {
             0.0
         };
@@ -201,7 +240,7 @@ impl<const SIZE: usize> Graph<SIZE> {
         line
     }
 
-    fn update_vertices(vertices: &mut [Vertex], line: &[f32]) {
+    fn update_vertices(geometry: &Geometry, vertices: &mut [Vertex], line: &[f32]) {
         assert!(!vertices.is_empty());
         assert!(vertices.len() % line.len() == 0);
 
@@ -209,41 +248,42 @@ impl<const SIZE: usize> Graph<SIZE> {
 
         for i in 0..vertices.len() / line.len() {
             for j in 0..line.len() {
-                vertices[i * line.len() + j].position[0] = i as f32 + Self::OFFSET_X as f32;
+                vertices[i * line.len() + j].position[0] = i as f32 + geometry.offset_x as f32;
             }
         }
 
         for i in 0..line.len() {
-            vertices[i].position[1] = line[i] + Self::OFFSET_Y as f32;
+            vertices[i].position[1] = line[i] + geometry.offset_y as f32;
         }
     }
 
     pub fn get_height(&self) -> usize {
-        Self::HEIGHT
+        self.geometry.height
     }
 
     pub fn get_height_30fps(&self) -> f32 {
-        if Self::FPS_LINES.len() >= 8 {
-            Self::FPS_LINES[6].position[1]
+        if self.geometry.fps_lines.len() >= 8 {
+            // self.geometry.fps_lines[6].position[1]
+            0.0
         } else {
             0.0
         }
     }
 
     pub fn get_height_60fps(&self) -> f32 {
-        Self::FPS_LINES[4].position[1]
+        self.geometry.fps_lines[4].position[1]
     }
 
     pub fn get_height_120fps(&self) -> f32 {
-        Self::FPS_LINES[2].position[1]
+        self.geometry.fps_lines[2].position[1]
     }
 
     pub fn get_width(&self) -> usize {
-        Self::WIDTH
+        self.geometry.width
     }
 
     pub fn get_nr_lines(&self) -> usize {
-        Self::NR_LINES
+        self.geometry.nr_lines
     }
 
     pub fn update_from_viewer_data(&mut self, data: &watch::WatchViewerData<SIZE>) {
@@ -255,7 +295,8 @@ impl<const SIZE: usize> Graph<SIZE> {
             return;
         }
 
-        let line = Self::create_line(last_update_time, update_time, watch_points);
-        Self::update_vertices(&mut self.vertices[Self::FPS_LINES.len()..], &line);
+        let fps_lines_les = self.geometry.fps_lines.len();
+        let line = Self::create_line(&self.geometry, last_update_time, update_time, watch_points);
+        Self::update_vertices(&self.geometry, &mut self.vertices[fps_lines_les..], &line);
     }
 }
