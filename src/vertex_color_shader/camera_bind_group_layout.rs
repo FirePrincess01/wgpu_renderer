1cd8de5ac2a087aec3bd5b2f30893b63121c0efa
FirePrincess01 <SarahSteinbrecher01@gmx.at>
2026-07-14 19:19:03 +0200

    added shadow map

--- src/vertex_color_shader/camera_bind_group_layout.rs
+++ src/vertex_color_shader/camera_bind_group_layout.rs
@@ -19,7 +19,18 @@ impl CameraBindGroupLayout {
                         min_binding_size: None,
                     },
                     count: None,
-                }],
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }
+                ],
                 label: Some("camera_bind_group_layout"),
             });
 
