b6e91466e7f51c06206fcf72602326fb683ee14f
FirePrincess01 <SarahSteinbrecher01@gmx.at>
2026-08-08 12:56:17 +0200

    add changes

--- src/vertex_color_shader/camera_bind_group_layout.rs
+++ src/vertex_color_shader/camera_bind_group_layout.rs
@@ -13,7 +13,7 @@ impl CameraBindGroupLayout {
                 entries: &[
                     wgpu::BindGroupLayoutEntry {
                         binding: 0,
-                        visibility: wgpu::ShaderStages::VERTEX,
+                        visibility: wgpu::ShaderStages::VERTEX | wgpu::ShaderStages::FRAGMENT,
                         ty: wgpu::BindingType::Buffer {
                             ty: wgpu::BufferBindingType::Uniform,
                             has_dynamic_offset: false,
