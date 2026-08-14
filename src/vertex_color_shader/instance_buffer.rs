274bd092a57d7a0bcfdf9ffcb38d861678d9b59a
cryonether <121761997+cryonether@users.noreply.github.com>
2026-08-14 17:11:57 +0200

    feature/run-neat-algo-in-seperate-thread (#24)
    Separates the physics thread from drawing thread for faster simulations

--- src/vertex_color_shader/instance_buffer.rs
+++ src/vertex_color_shader/instance_buffer.rs
@@ -5,7 +5,7 @@ use wgpu::util::DeviceExt;
 
 pub struct InstanceBuffer<TInstance> {
     buffer: wgpu::Buffer,
-    _size: u32,
+    _size: usize,
     phantom: std::marker::PhantomData<TInstance>,
 }
 
@@ -20,7 +20,7 @@ where
             usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
         });
 
-        let _size = instances.len() as u32;
+        let _size = instances.len();
 
         Self {
             buffer,
@@ -45,7 +45,16 @@ where
         render_pass.set_vertex_buffer(slot, self.buffer.slice(..));
     }
 
-    pub fn size(&self) -> u32 {
+    pub fn len(&self) -> usize {
         self._size
+    }
+
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn size(&self) -> u32 {
+        self._size as u32
     }
 }
